f157b78556d89bec8ab850dd59f493c94a980be6
GemsNS <Bytecode2015@gmail.com>
2026-04-03 23:48:31 -0300

    .

--- assets/js/social-feed.js
+++ assets/js/social-feed.js
@@ -1,12 +1,10 @@
 async function initSocialFeeds() {
-  const homeFacebookEmbed = document.querySelector("[data-facebook-feature-embed]");
+  const homeFacebookImage = document.querySelector("[data-facebook-feature-image]");
   const homeFacebookLink = document.querySelector("[data-facebook-feature-link]");
   const facebookHighlights = document.querySelector("[data-facebook-highlights]");
-  const facebookFeedEmbed = document.querySelector("[data-facebook-feed-embed]");
-  const blogFeedEmbed = document.querySelector("[data-pastor-blog-embed]");
   const blogStatus = document.querySelector("[data-pastor-blog-status]");
 
-  if (!homeFacebookEmbed && !facebookHighlights && !blogFeedEmbed && !facebookFeedEmbed) {
+  if (!homeFacebookImage && !facebookHighlights && !blogStatus && !homeFacebookLink) {
     return;
   }
 
@@ -17,20 +15,12 @@ async function initSocialFeeds() {
 
     const feature = social.facebookFeatured || {};
     const fbUrl = feature.videoUrl || "https://www.facebook.com/";
-    const encodedFeatureUrl = encodeURIComponent(fbUrl);
 
-    if (homeFacebookEmbed) {
-      homeFacebookEmbed.src =
-        "https://www.facebook.com/plugins/video.php?href=" + encodedFeatureUrl + "&show_text=true&width=560";
+    if (homeFacebookImage) {
+      homeFacebookImage.src = feature.thumbnail || homeFacebookImage.src;
     }
     if (homeFacebookLink) {
       homeFacebookLink.href = fbUrl;
-    }
-
-    if (facebookFeedEmbed) {
-      const groupUrl = encodeURIComponent((data.placeholders && data.placeholders.links && data.placeholders.links.facebookGroup) || "https://www.facebook.com/");
-      facebookFeedEmbed.src =
-        "https://www.facebook.com/plugins/page.php?href=" + groupUrl + "&tabs=timeline&width=500&height=620&small_header=false&adapt_container_width=true&hide_cover=false&show_facepile=true";
     }
 
     if (facebookHighlights) {
@@ -49,9 +39,6 @@ async function initSocialFeeds() {
     }
 
     const blog = social.pastorBlog || {};
-    if (blogFeedEmbed) {
-      blogFeedEmbed.src = blog.url || "https://anthonyinspiration.com/";
-    }
     if (blogStatus) {
       blogStatus.textContent = blog.statusNote || "Live blog feed is connected.";
     }
