4fa797d4fdde0e84585b2526a499c60c5afeba06
GemsNS <Bytecode2015@gmail.com>
2026-04-16 19:09:10 -0300

    ';;;

--- assets/js/site.js
+++ assets/js/site.js
@@ -624,7 +624,7 @@
   const headerVideoIndexKey = "ncc_header_video_index";
   const headerManifestUrl = "./assets/headersrc/manifest.json";
   const headerPageMapUrl = "./assets/headersrc/page-headers.json";
-  const headerVideoFallback = "./assets/headersrc/DigitalWavesSummerWelcomeHD_PRV.mp4";
+  const headerVideoFallback = "./assets/headersrc/DigitalWavesSummerSunsetHD_PRV.mp4";
 
   function headerVideoMime(src) {
     const lower = (src || "").toLowerCase();
